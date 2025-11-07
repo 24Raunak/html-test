bd8a361e30b7a5ac8f3641ad80108f61ffb08e56
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-11-07 09:06:10 +0530

    few changes

---
+++ main/scripts/main.js
@@ -0,0 +1,13 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+ const mySrc = myImage.getAttribute("src");
+ if(mySrc === "images/image1.jpg") {
+  myImage.setAttribute("src", "images/image2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/image1.jpg");
+    }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
