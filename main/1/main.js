a37225df2fa94f938e5843679dc03626fec2a05a
24Raunak <79973936+24Raunak@users.noreply.github.com>
2026-02-15 07:53:11 +0530

    updated boostrap files

--- main/1/main.js
+++ main/1/main.js
@@ -1,11 +1,11 @@
 const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
- const mySrc = myImage.getAttribute("src");
- if(mySrc === "images/image1.jpg") {
-  myImage.setAttribute("src", "images/image2.jpg");
-  } else {
-    myImage.setAttribute("src", "images/image1.jpg");
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/image1.jpg") {
+        myImage.setAttribute("src", "images/image2.jpg");
+    } else {
+        myImage.setAttribute("src", "images/image1.jpg");
     }
 });
 
